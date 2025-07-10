3bcad50831fcb7bdc336b888cfe91afa939e4919
yurimaurino <ymaurinoborges@gmail.com>
2025-07-10 14:33:37 -0300

    adicionando funcionalidade ao form

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
+
+const sendForm = (event) => {
+  event.preventDefault()
+
+}
+form.addEventListener('submit', sendForm)
